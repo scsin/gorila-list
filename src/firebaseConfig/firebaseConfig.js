4fd822392fe69bbebc13956da77f676ebd308532
scsin <saracsin26@gmail.com>
2020-01-26 19:02:35 -0300

    config port, firebaseConfig

---
+++ src/firebaseConfig/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC5vMx9qcc9xTuajpQiVajydoIIQlIF1L8",
+    authDomain: "gorila-list.firebaseapp.com",
+    databaseURL: "https://gorila-list.firebaseio.com",
+    projectId: "gorila-list",
+    storageBucket: "gorila-list.appspot.com",
+    messagingSenderId: "204780133443",
+    appId: "1:204780133443:web:3cce5c88c2b8fc66c648f6"
+};
+
+export default firebaseConfig;
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import axios from 'axios';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+ServiceWorker.unregister();
 
 const port = axios.create({baseURL: process.env.REACT_APP_GORILA_LIST})
 
