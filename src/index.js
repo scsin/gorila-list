6099d830fbb0ada021db2643db4842d341332591
scsin <saracsin26@gmail.com>
2020-01-27 10:59:46 -0300

    add procfile

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import axios from 'axios';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-ServiceWorker.unregister();
-
 const port = axios.create({baseURL: process.env.REACT_APP_GORILA_LIST})
 
 export default port;
